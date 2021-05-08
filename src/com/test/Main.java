48a78838c54edc577b97114cd0a4153bad59872e
berumoris <berumoris@gmail.com>
2021-05-08 12:01:33 +0300

    Initial commit2

---
+++ src/com/test/Main.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Вроде работает");
+
+        // write your code here
+    }
+}
